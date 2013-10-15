be6f8cf9b79569cca3d9f199877ab72e6b2024f3
arseniy <arsenyzp@gmail.com>
2013-10-15 14:35:05 +0300

    просмотр статей по категориям и по тегам

--- article/urls.py
+++ article/urls.py
@@ -4,8 +4,8 @@ from article import views
 urlpatterns = patterns('',
 	url(r'^$', views.ListArticlesView.as_view(), name='index'),
 	url(r'^(?P<slug>\w+)/$', views.DetailView.as_view(), name='detail'),
-	url(r'^(?P<slug>\w+)/tag/$', views.ArticlesForTag.as_view(), name='tag'),
-	url(r'^(?P<slug>\w+)/categories/$', views.ArticlesForCategories.as_view(), name='categories'),
+	url(r'^(?P<slug>\w+)/tag/$', views.forTag, name='tag'),
+	url(r'^(?P<slug>\w+)/catigory/$', views.forCategories, name='categories'),
 	#url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 	#url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- article/views.py
+++ article/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
-from article.models import Articles, TagArticles
+from article.models import Articles, TagArticles, CategoriesArticles
 from django.views import generic
 from django.utils import timezone
 
@@ -20,16 +20,10 @@ class DetailView(generic.DetailView):
 	template_name = "article/detail.html"
 	context_object_name = "article"
 
-class ArticlesForTag(generic.ListView):
-	model = Articles
-	template_name = "article/list.html"
-	context_object_name = "article_list"
+def forTag(request,slug):
+	article_list = Articles.objects.filter(tagList=TagArticles.objects.filter(slug=slug)).order_by('-date_publish')[:5]
+	return render(request, 'article/list.html', {'article_list': article_list})
 
-	def get_queryset(self):
-		content = Articles.objects.filter(tagList=slug).order_by('-date_publish')[:5]
-		return content
-
-class ArticlesForCategories(generic.ListView):
-	model = Articles
-	template_name = "article/list.html"
-	context_object_name = "article_list"		
+def forCategories(request,slug):
+	article_list = Articles.objects.filter(category_id=CategoriesArticles.objects.filter(slug=slug)).order_by('-date_publish')[:5]
+	return render(request, 'article/list.html', {'article_list': article_list})
